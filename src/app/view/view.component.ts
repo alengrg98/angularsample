21afaf995944b12388d6031b967200f1355570b9
alengrg98 <alengeorgeshibu@cs.ajce.in>
2019-08-23 13:28:32 +0530

    commit 3

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  name: String;
+  age: Number;
+  email: String;
+  address;
+  hobbies : String[];
+  projects : any[]=[];  //initialize with null array always for database
+  constructor() { 
+    this.name ="Alen";
+    this.age = 21;
+    this.email = "alengrg98@gmail.com";
+    this.address = {
+      houseNo : "ABC",
+      street : "abc",
+      dist : "ekm"
+    };
+    this.hobbies = ["reading", "listening to music"];
+    this.projects = [
+    {
+      name : "Project1",
+      duration : "5 weeks",
+      endDate : "30/10/19"
+    },
+    {
+      name : "Project2",
+      duration : "10 weeks",
+      endDate : "25/12/19"
+    }];
+  }
 
   ngOnInit() {
   }
 
+  addProject(){
+    let data = 
+      {
+        name : "Project1",
+        duration : "5 weeks",
+        endDate : "30/10/19"
+      };
+    this.projects.push(data);
+  }
 }
